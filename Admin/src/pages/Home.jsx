5bd2d4c33808a5e808100f7bf2f188ab8fec44aa
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2022-10-28 22:56:40 +0300

    README updated, old files delted to be moved to other branches

--- Admin/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div>Librarybot</div>
-  )
-}
-
-export default Home
--- Admin/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from "./Home";
-export { default as ShowQuizes } from "./ShowQuizes";
-export { default as CreateQuiz } from "./CreateQuiz";
