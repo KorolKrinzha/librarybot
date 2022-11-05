4dde60868670b44ea7870fafc1cc6de7ead41094
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2022-11-05 16:19:12 +0300

    Added first working routes

---
+++ main.py
@@ -0,0 +1,152 @@
+import time 
+from functools import wraps
+from flask import Flask, request, send_from_directory, abort, Response, jsonify
+import mysql.connector
+import env
+import json
+# from DB_calls import check_admin, add_user, check_quiz_type,  add_quiz, show_all, show_all_preview, show_quiz_with_type, show_quiz_no_type, delete_quiz
+from DB_calls import add_user, show_quiz_no_type, check_answer,show_userscore
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+# def admin_role(f):
+#     @wraps(f)
+#     def wrap(*args, **kwargs):
+#         try:
+#             session_token = request.cookies['session_token']
+#             if check_admin(session_token): return f(*args, **kwargs)
+#             else: return abort(403)
+#         except Exception as e: 
+#             print(e)
+#             return abort(403)
+#     return wrap
+
+# Проверка API
+@app.route("/api", methods=['GET'])
+def api():
+    return "Hello from API"
+
+# ОБЫЧНЫЙ ПОЛЬЗОВАТЕЛЬ
+
+# Добавление пользователя
+@app.route("/api/adduser",methods=['POST'])
+def api_adduser():
+    
+    user_id = request.args.get(key='user_id')
+    lastname = request.args.get(key='lastname')
+    firstname = request.args.get(key='firstname')
+    username = request.args.get(key='username')
+    
+    try:
+        add_user(user_id,lastname,firstname,username)
+        return Response(response='Пользователь успешно зарегистрирован', status=200)
+
+    except Exception as e:
+        print(e) 
+        return Response(response='Ошибка при регистрировании пользователя', status=500)
+    
+
+# Показ ивента
+@app.route("/api/quiz",methods=['GET'])
+def api_quiz():
+    quiz_id = request.args.get(key='quiz_id')
+    quiz_data = show_quiz_no_type(quiz_id)
+    Response(
+        response=quiz_data,
+        status=200,
+        mimetype='application/json')
+
+    return quiz_data
+
+# Проверка ответа на ивент пользователя
+@app.route("/api/answer",methods=['POST'])
+def api_answer():
+    quiz_id = request.args.get(key='quiz_id')
+    user_id = request.args.get(key='user_id')
+    answer = request.args.get(key='answer')
+    
+    try:
+        correct, reply = check_answer(quiz_id,quiz_type,user_id,answer)
+        if correct:
+            return Response(response=reply, status=200)
+        else:
+            return Response(response=reply, status=200)
+    except: 
+        return Response(response='Ошибка при получении ответа', status=500)
+
+
+# Просмотр счета 
+@app.route("/api/userscore",methods=['GET'])	
+def api_userscore():
+    user_id = request.args.get(key='user_id')
+    try:
+        userscore = show_userscore(user_id)
+        return Response(response=userscore, status=200, mimetype='application/json')
+    except:
+        return Response(response='Ошибка при получении данных', status=500)
+
+
+# ПОЛЬЗОВАТЕЛЬ-АДМИН 
+@app.route("/api/admin/showusers", methods=['GET'])
+def api_showusers():
+    users = show_all_users()
+    return users
+
+
+# # ДОБАВЛЕНИЕ ИВЕНТА ЛЮБОГО УСТАНОВЛЕННОГО ТИПА - АДМИН КЛИЕНТ
+# @app.route("/api/admin/createquiz", methods=['POST'])
+# # @admin_role
+# def api_admin_createquiz():
+#     post_data = request.data
+#     data_json = json.loads(post_data.decode('utf-8'))
+#     print(data_json)
+#     quiz_type = data_json['quiz_type']
+    
+#     try:
+#         add_quiz(quiz_type, data_json)
+#     except Exception as error: print(error)
+#     return ''
+
+
+# @app.route("/api/admin/quiz_with_type", methods=['GET'])
+# def api_admin_quiz_with_type():
+#     quiz_type = request.args.get('quiz_type')
+#     quiz_id = request.args.get('quiz_id')
+#     quiz_data = show_quiz_with_type(quiz_id, quiz_type)
+
+    
+#     return Response(response=quiz_data,
+#                     status=200,
+#                     mimetype='application/json')
+
+# # !!! УДАЛЕНИЕ КВИЗА - АДМИН КЛИЕНТ
+# @app.route("/api/admin/deletequiz", methods=['POST'])
+# def api_admin_deletequiz():
+
+#     post_data = request.data
+#     data_json = json.loads(post_data.decode('utf-8'))
+#     quiz_id = data_json['quiz_id']
+#     print(quiz_id, "11111")
+
+#     try:
+#         delete_quiz(quiz_id)
+#         return Response(status=200)
+#     except Exception as e:
+#         return Response(response=str(e),status=500)
+
+# # !!! ПОКАЗ ВСЕХ ИВЕНТОВ - АДМИН КИЕНТ
+# @app.route("/api/admin/showall", methods=['GET'])
+# def api_admin_showall():
+    
+#     json_data = json.dumps(show_all(),  ensure_ascii=False)   
+       
+#     return jsonify(quizes=json_data)
+
+
+
+if __name__ == 'main':
+    app.run(debug=True, host='0.0.0.0', port=6000)
+
